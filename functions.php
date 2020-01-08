cbedf305a78a305e7db7dd53dda4b15ff6602109
chowdigital <sean@chowdigital.com>
2020-01-08 11:57:35 +0000

    oothstate.js working but a bit glitchy on menus
    Signed-off-by: chowdigital <sean@chowdigital.com>

--- functions.php
+++ functions.php
@@ -128,9 +128,7 @@ function lostboys2020_scripts() {
 
 	wp_enqueue_script( 'lostboys2020-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'smootstate-js', 'https://cdnjs.cloudflare.com/ajax/libs/smoothState.js/0.7.2/jquery.smoothState.min.js', array( 'jquery' ), '0.7.2' );
-
-	wp_enqueue_script( 'script-js', trailingslashit( get_stylesheet_directory_uri() ) . '/js/smoothStateScript.js' , array( 'jquery', 'smootstate-js' ), '1.0.0', true );
+	
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -164,4 +162,15 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+/**
+	 * Enqueue required scripts.
+	 */
+	add_action( 'wp_enqueue_scripts', function() {
+		
+	
+		wp_enqueue_script( 'smootstate-js', 'https://cdnjs.cloudflare.com/ajax/libs/smoothState.js/0.7.2/jquery.smoothState.min.js', array( 'jquery' ), '0.7.2' );
+		wp_enqueue_script( 'script-js', trailingslashit( get_stylesheet_directory_uri() ) . 'assets/js/script.min.js' , array( 'jquery', 'smootstate-js' ), '1.0.0', true );
+		
+	} );
 
--- header.php
+++ header.php
@@ -21,31 +21,13 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<wrapper id="wrapper">
-		<div class="scanlines">
-			
-			<div id="page" class="site">
+	<div id="page" class="site">
+			<wrapper id="wrapper">
+			<div class="scanlines">
 				<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'lostboys2020' ); ?></a>
 
 				<header id="masthead" class="site-header scene_element--fadein">
-					<div class="site-branding">
-			<!--<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$lostboys2020_description = get_bloginfo( 'description', 'display' );
-			if ( $lostboys2020_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $lostboys2020_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+			
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
