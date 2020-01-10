a50418c2ce2f673f060fbbfb058ed1c0a02376f0
chowdigital <sean@chowdigital.com>
2020-01-10 11:51:37 +0000

    creating home page stage 1

---
+++ template-home.php
@@ -0,0 +1,20 @@
+<?php /* Template Name: Lost Boys Homepage */ get_header(); ?>
+<section class="pizza">
+	<img src="<?php echo get_template_directory_uri(); ?>/img/pizza1.png" alt="Pizza">
+	<div class="line1"><h2>Black Charcoal Pizza</h2></div>
+	<div class="line2"><h2>Killer Cocktails</h2></div>
+	<div class="line3"><h2>All served with a banging 80s playlist</h2></div>
+	
+
+
+
+</section>
+<section class="instagram"></section>
+<section class="contact"></section>
+<section class="blog"></section>
+<section class="menu"></section>
+<section class="promo"></section>
+
+
+
+<?php get_footer(); ?>
