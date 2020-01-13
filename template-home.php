7e746175c581e504bcf4c6ca98d0c2294c92bed5
chowdigital <sean@chowdigital.com>
2020-01-13 15:06:38 +0000

    menu section half done

--- template-home.php
+++ template-home.php
@@ -61,8 +61,98 @@ wp_reset_postdata();
  
 ?>
 </section>
+
+
 <section class="menu">
+
+
+
+					<h1><span class="main-text skew">
+							Menu
+	    						<span class="back-text glitch">Menu</span>
+	  					</span>
+	  				</h1>
+				
+			
+					
+				
+		
+
+
 	
+
+
+			
+		<div class="leftPicBox">
+				
+			<img src="<?php echo get_template_directory_uri(); ?>/img/pizza2cutout.png" alt="">
+
+			</div>
+
+		
+		<div class="pizzaMenu">
+			
+			
+
+	<a href="<?php the_field('MAIN_MENU'); ?>" class="menuLink">
+	  <img src="<?php echo get_template_directory_uri(); ?>/img/menu/pizza.jpg" alt="Avatar">
+	  <div class="bottom">
+	  	<h4>MAIN MENU</h4>
+	   <!--<div class="text"><h1>PIZZA</h1></div>-->
+	  </div>
+	</a>
+
+	<a href="<?php the_field('BRUNCH'); ?>" class="menuLink">
+	  <img src="<?php echo get_template_directory_uri(); ?>/img/menu/brunch.jpg" alt="Avatar">
+	  <div class="bottom">
+	  	<h4>BRUNCH</h4>
+	   <!--<div class="text"><h1>PIZZA</h1></div>-->
+	  </div>
+	</a>
+
+	<a href="<?php the_field('DESSERTS'); ?>" class="menuLink">
+	  <img src="<?php echo get_template_directory_uri(); ?>/img/menu/dessert.jpg" alt="Avatar">
+	  <div class="bottom">
+	  	<h4>DESSERTS</h4>
+	   <!--<div class="text"><h1>PIZZA</h1></div>-->
+	  </div>
+	</a>
+
+	<a href="<?php the_field('DRINKS'); ?>" class="menuLink">
+	  <img src="<?php echo get_template_directory_uri(); ?>/img/menu/drink.jpg" alt="Avatar">
+	  <div class="bottom">
+	  	<h4>DRINKS</h4>
+	   <!--<div class="text"><h1>PIZZA</h1></div>-->
+	  </div>
+	</a>
+
+	<a href="<?php the_field('PARTY'); ?>" class="menuLink">
+	  <img src="<?php echo get_template_directory_uri(); ?>/img/menu/party.jpg" alt="Avatar">
+	  <div class="bottom">
+	  	<h4>PARTY</h4>
+	   <!--<div class="text"><h1>PIZZA</h1></div>-->
+	  </div>
+	</a>
+	
+
+
+
+	  				  </div>
+		<!--<div class="imageBox">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/pizza1cutout.png" alt="">
+
+			</div>-->
+
+								<!--<?php the_field('pizzaIntro'); ?>
+
+								<?php the_field('pizza'); ?>-->
+
+
+
+
+		</div>
+
+
 </section>
 <section class="promo"></section>
 
